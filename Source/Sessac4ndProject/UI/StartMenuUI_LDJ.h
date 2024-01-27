18d7527f8b9a7efe2bdc45e53af63aaaf138354d
myeonghoon <mhoo999@naver.com>
2024-01-27 19:55:53 +0900

    YMH:0127_be resolved

---
+++ Source/Sessac4ndProject/UI/StartMenuUI_LDJ.h
@@ -0,0 +1,68 @@
+// zombie must die
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "StartMenuUI_LDJ.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SESSAC4NDPROJECT_API UStartMenuUI_LDJ : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	// ----- Override 함수 -----
+	virtual void NativeConstruct() override;
+	// ----- Override 함수 -----
+
+	// ----- 함수 -----
+	UFUNCTION()
+	void ClickCreateSession();
+
+	UFUNCTION()
+	void ClickJoinSession();
+	
+	UFUNCTION()
+	void ClickBack();
+	
+	UFUNCTION()
+	void ClickPlay();
+	
+	UFUNCTION()
+	void AddSlotWidget(const struct FSessionInfo& SessionInfo);
+	
+	UFUNCTION()
+	void OnJoinButtonEnabled(bool bIsSearching);
+	// ----- 함수 -----
+
+	// ----- 참조 변수 -----
+	UPROPERTY()
+	class UZMDGameInstance_LDJ* GI;
+	// ----- 참조 변수 -----
+
+	// ----- 변수 -----
+	UPROPERTY(meta=(BindWidget))
+	class UButton* Btn_CreateSession;
+
+	UPROPERTY(meta=(BindWidget))
+	class UButton* Btn_JoinSession;
+
+	UPROPERTY(meta=(BindWidget))
+	class UButton* Btn_Back;
+
+	UPROPERTY(meta=(BindWidget))
+	class UButton* Btn_Play;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta=(BindWidget))
+	class UScrollBox* ScrollBox_RoomList;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta=(BindWidget))
+	class UScrollBox* ScrollBox_CharacterList;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<class USessionSlotWidget_LDJ> SessionSlotWidget;
+	// ----- 변수 -----
+};
---
+++ Source/Sessac4ndProject/UI/TitleMenuUI_LDJ.h
@@ -0,0 +1,58 @@
+// zombie must die
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TitleMenuUI_LDJ.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SESSAC4NDPROJECT_API UTitleMenuUI_LDJ : public UUserWidget
+{
+	GENERATED_BODY()
+	UTitleMenuUI_LDJ(const FObjectInitializer& ObjectInitializer);
+	
+public:
+	// ----- Override 함수 -----
+	virtual void NativeConstruct() override;
+	// ----- Override 함수 -----
+
+	// ----- 함수 -----
+	UFUNCTION()
+	void Click_Start();
+	
+	UFUNCTION()
+	void Click_Exit();
+
+	UFUNCTION()
+	void Hover_Start();
+
+	UFUNCTION()
+	void UnHover_Start();
+
+	UFUNCTION()
+	void Hover_Exit();
+
+	UFUNCTION()
+	void UnHover_Exit();
+	// ----- 함수 -----
+
+	// ----- 참조 변수 -----
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<class UStartMenuUI_LDJ> StartMenuFac;
+
+	UPROPERTY()
+	class UStartMenuUI_LDJ* StartMenu;
+	// ----- 참조 변수 -----
+
+	// ----- 변수 -----
+	UPROPERTY(meta=(BindWidget))
+	class UButton* Btn_Start;
+
+	UPROPERTY(meta=(BindWidget))
+	class UButton* Btn_Exit;
+	// ----- 변수 -----
+};
