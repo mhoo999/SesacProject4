660a51afd92d04cb80f4509723e4badebb9495d6
ldj95412 <144770957+ldj95412@users.noreply.github.com>
2024-01-29 10:42:22 +0900

    KJY:0129_ItemActor 및 좀비 콜리전 수정

---
+++ Source/Sessac4ndProject/Actor/ItemActor_KJY.cpp
@@ -0,0 +1,27 @@
+// zombie must die
+
+
+#include "Actor/ItemActor_KJY.h"
+
+// Sets default values
+AItemActor_KJY::AItemActor_KJY()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AItemActor_KJY::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AItemActor_KJY::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
