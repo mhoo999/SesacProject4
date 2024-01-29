0a567e2661c1dae346dd1bdcf0b7a4a40f21ff3e
ldj95412 <144770957+ldj95412@users.noreply.github.com>
2024-01-29 21:51:43 +0900

    KJY:0129_FastZombie_KJY 캐릭터 속도 조정

--- Source/Sessac4ndProject/Character/Enemy/FastZombie_KJY.cpp
+++ Source/Sessac4ndProject/Character/Enemy/FastZombie_KJY.cpp
@@ -3,17 +3,19 @@
 
 #include "Character/Enemy/FastZombie_KJY.h"
 
+#include "FastZombieFSM_KJY.h"
 #include "GameFramework/CharacterMovementComponent.h"
 
 
 AFastZombie_KJY::AFastZombie_KJY()
 {
-	GetCharacterMovement()->MaxWalkSpeed = 600;
+	
 }
 
 void AFastZombie_KJY::BeginPlay()
 {
 	Super::BeginPlay();
+	
 }
 
 void AFastZombie_KJY::Tick(float DeltaTime)
