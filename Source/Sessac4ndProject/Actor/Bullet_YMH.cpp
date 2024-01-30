1b014602bee864f5a67c01ed6f745d61684e007e
myeonghoon <mhoo999@naver.com>
2024-01-30 15:42:28 +0900

    YMH:0130_총알 앞으로만 나가기

--- Source/Sessac4ndProject/Actor/Bullet_YMH.cpp
+++ Source/Sessac4ndProject/Actor/Bullet_YMH.cpp
@@ -31,12 +31,12 @@ void ABullet_YMH::BeginPlay()
 	// auto player = Cast<APlayerBase_YMH>(newOwner);
 	// FindOwner();
 
-	pc = Cast<APlayerController_YMH>(GetWorld()->GetFirstPlayerController());
-	auto player = Cast<APlayerBase_YMH>(pc->GetPawn());
-	
-	destination = player->FireComp->bulletDropPoint;
-	direction = destination - GetActorLocation();
-	direction.Normalize();
+	// pc = Cast<APlayerController_YMH>(GetWorld()->GetFirstPlayerController());
+	// auto player = Cast<APlayerBase_YMH>(pc->GetPawn());
+	//
+	// destination = player->FireComp->bulletDropPoint;
+	// direction = destination - GetActorLocation();
+	// direction.Normalize();
 	
 	FTimerHandle deathTime;
 	GetWorld()->GetTimerManager().SetTimer(deathTime, FTimerDelegate::CreateLambda([&]
@@ -50,8 +50,8 @@ void ABullet_YMH::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 	
 	FVector p0 = GetActorLocation();
-	FVector v = direction * bulletSpeed;
-	// FVector v = GetActorForwardVector() * bulletSpeed;
+	// FVector v = direction * bulletSpeed;
+	FVector v = GetActorForwardVector() * bulletSpeed;
 	SetActorLocation(p0 + v * DeltaTime);
 }
 
